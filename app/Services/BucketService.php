dcc85b7440932648f2aa3df201d0069b413d1d9a
Sem28 <samuelzakaria28@gmail.com>
2024-03-02 20:55:29 +0700

    commit adding services

--- app/Services/BucketService.php
+++ app/Services/BucketService.php
@@ -3,18 +3,45 @@
 namespace App\Services;
 
 use App\Models\Bucket;
+use http\Env\Response;
 use Illuminate\Database\Eloquent\Collection as EloquentCollection;
 
 class BucketService
 {
-    public function __construct(public int $id)
+    public function __construct(public int $userId, public int $id)
     {
         if (auth()->check()) {
-            $this->id = auth()->user()->id;
+            $this->userId = auth()->user()->id;
         }
+    }
+
+    public function getData(): EloquentCollection
+    {
+        return Bucket::where('users_id')->with('events')->get();
     }
     public function getDataWithPricesEvents(): EloquentCollection
     {
-        return Bucket::where('users_id', $this->id)->with('prices', 'events')->get();
+        return Bucket::where('users_id', $this->userId)->with('prices', 'events')->get();
+    }
+
+    //Delete Item on Bucket
+
+    public function removeItem()
+    {
+        try {
+            $item = Bucket::find('id', $this->id);
+            $item->delete();
+            $data=[
+                'status'=>'1',
+                'message'=>'success'
+            ];
+        }
+        catch (error) {
+            $data=[
+                'status'=>'0',
+                'message'=>'failed'
+            ];
+        }
+            return response()->json($data);
     }
 }
