0072a8c84bffc990e61917d694d87efbd5302efb
Jian Jeraus Young <jianjerausyoung37@gmail.com>
2024-04-02 19:12:35 +0700

    about us

--- database/factories/InfoFactory.php
+++ database/factories/InfoFactory.php
@@ -17,8 +17,8 @@ class InfoFactory extends Factory
     public function definition(): array
     {
         return [
-                'infoName' => "Who is Simposium Manado?",
-                "info"=> $this->faker->paragraph(15),
+                'infoName' => "What is Event Hub",
+                "info"=> "A platform that makes it easy for users from all walks of life to register and access upcoming events. From seminars to concerts, Event HUB is here to serve everything. With just an email, you can register for interesting events. Come on, what are you waiting for? Register and enjoy Event HUB's events!",
         ];
     }
 }
