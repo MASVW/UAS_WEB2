891d2190435177a6cca6aceb282afebd8c5eff50
Vinson Andriano <vinsonandriano@gmail.com>
2024-04-30 19:13:11 +0700

    CheckoutConfirmation3

--- app/Livewire/CheckoutConfirmation.php
+++ app/Livewire/CheckoutConfirmation.php
@@ -6,6 +6,15 @@ use Livewire\Component;
 
 class CheckoutConfirmation extends Component
 {
+    public $halaman = 1;
+
+    public function nextPage(){
+        $this->halaman++;
+    }
+
+    public function prevPage(){
+        $this->halaman--;
+    }
     public function render()
     {
         return view('livewire.checkout-confirmation');
